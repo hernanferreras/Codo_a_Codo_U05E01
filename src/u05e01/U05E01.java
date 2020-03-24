0dcd1ed26a1f0e6924328ec8c3688c200af1ca98
maxher73 <maxher73@yahoo.com.ar>
2020-03-24 12:20:30 -0300

    Unidad 5 Ejercicio 1 de la practica de Codo a Codo

---
+++ src/u05e01/U05E01.java
@@ -0,0 +1,11 @@
+
+package u05e01;
+
+public class U05E01 {
+    public static void main(String args[]) {
+      HolaMundo();
+    }
+    static void HolaMundo(){
+        System.out.println("Hola Mundo");
+    }
+}
